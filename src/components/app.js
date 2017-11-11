529ea01d429ef3d5cfb3777921aa82d3662335e6
William <he.william@yahoo.com>
2017-11-10 18:20:36 -0800

    added playerList

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import SearchBar from '../containers/searchBar';
+import PlayerList from '../containers/playerList'
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <SearchBar />
+        <PlayerList />
       </div>
     );
   }
