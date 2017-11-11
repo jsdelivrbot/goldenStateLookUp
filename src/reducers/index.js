ddf59c29e5a16ff7bf372cf2e73a97018fbf33eb
William <he.william@yahoo.com>
2017-11-10 18:22:25 -0800

    added playerreducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import PlayerReducer from '../reducers/reducerPlayer';
 
 const rootReducer = combineReducers({
-    state: (state = {}) => state
+    players: PlayerReducer
 });
 
 export default rootReducer;
